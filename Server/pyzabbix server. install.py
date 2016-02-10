c868ba77c770961209f61113ad22bc0b5ba379f8
Diego Bernardes <diegobernardesctba@gmail.com>
2016-02-10 09:05:22 -0200

    Zabbix Server Install

---
+++ Server/pyzabbix server. install.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+#Testado usando python 3 rodando sobre Debian 8.2
+
+import subprocess, os
+print('''
+Antes de prosseguir, configure um IP fixo para o servidor
+# vi /etc/networ/interfaces
+auto <interface>
+    iface <interface> inet static
+    address <ip>
+    netmask <mascara>
+    gateway <gateway>
+:wq''')
+
+os.system('apt-get update')
+
+
+print('Realizando a instalação do MySQL, informe uma senha de root para o mysql quando for solicitado')
+os.system('apt-get install mysql-server')
+
+
+print('Baixando e instalando o zabbix 2.4')
+os.system('wget -c http://repo.zabbix.com/zabbix/2.4/debian/pool/main/z/zabbix-release/zabbix-release_2.4-1+jessie_all.deb')
+os.system('dpkg -i zabbix-release_2.4-1+jessie_all.deb')
+os.system('apt-get update')
+os.system('apt-get install zabbix-server-mysql zabbix-frontend-php zabbix-agent zabbix-get zabbix-sender')
+print('Alterando o arquivo apache.conf em /etc/zabbix/')
+os.system('cp -f config/apache.conf /etc/zabbix/')
+print('Alterando o arquivo php.ini em /etc/php5/apache2/')
+os.system('cp -f config/php.ini /etc/php5/apache2/')
+os.system('service apache2 restart')
